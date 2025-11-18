59485b3a9b512149ab076c8bad1e5d3fa9012d99
a.anuarbekov <a.anuarbekov@aifc.kz>
2025-11-18 17:16:01 +0500

    Product: Implemented endpoints for Product.

---
+++ src/main/java/com/backbone/phalanx/product/controller/ProductAdminController.java
@@ -0,0 +1,46 @@
+package com.backbone.phalanx.product.controller;
+
+
+import com.backbone.phalanx.product.dto.ProductRequestDto;
+import com.backbone.phalanx.product.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/public/admin/product")
+@Tag(name = "Product for Admin", description = "Administration operations with products")
+@RequiredArgsConstructor
+@Slf4j
+public class ProductAdminController {
+
+    private final ProductService productService;
+
+    @PostMapping("/add")
+    @Operation(summary = "Add the product", description = "Post request to add the product")
+    public ResponseEntity<Void> add(@RequestBody ProductRequestDto productRequestDto) {
+        productService.createProduct(productRequestDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    @Operation(
+            summary = "Update information of the product",
+            description = "Put request to update information of the product"
+    )
+    public ResponseEntity<Void> update(@RequestBody ProductRequestDto productRequestDto) {
+        productService.updateProduct(productRequestDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/remove/{externalId}")
+    @Operation(summary = "Remove the product", description = "Post request to remove the product")
+    public ResponseEntity<Void> remove(@PathVariable("externalId") String externalId) {
+        productService.deleteProduct(externalId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/backbone/phalanx/product/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.backbone.phalanx.product.controller;
+
+import com.backbone.phalanx.product.dto.ProductFilterRequestDto;
+import com.backbone.phalanx.product.dto.ProductFilterResponseDto;
+import com.backbone.phalanx.product.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/public/product")
+@Tag(name = "Product", description = "Operations with products")
+@RequiredArgsConstructor
+@Slf4j
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    @Operation(summary = "Retrieve all products", description = "Get request to retrieve all products")
+    public ProductFilterResponseDto getAllProductsByFiltering(
+            @RequestBody ProductFilterRequestDto productFilterRequestDto
+    ) {
+        return productService.getAllProductsByFiltering(productFilterRequestDto);
+    }
+}
