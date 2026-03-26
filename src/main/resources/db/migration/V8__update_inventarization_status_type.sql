2fcd605b401f3612b85f94784d41c4f561390e5d
Alisher <alisher.anuarbekov@gmail.com>
2026-03-26 18:46:27 +0500

    AI: Fixed during launch of a system.

--- src/main/resources/db/migration/V8__update_inventarization_status_type.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE inventarization ALTER COLUMN status TYPE INTEGER USING (
-    CASE status
-        WHEN 'CREATED' THEN 1
-        WHEN 'IN_PROGRESS' THEN 2
-        WHEN 'COMPLETED' THEN 3
-        ELSE 1
-    END
-);
